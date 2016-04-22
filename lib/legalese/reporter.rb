640a1fdb96bfcb87516eb12818ed423aa2fcf410
Aidan Feldman <aidan.feldman@gsa.gov>
2016-04-22 01:47:21 -0400

    simplify methods in Reporter

--- lib/legalese/reporter.rb
+++ lib/legalese/reporter.rb
@@ -15,40 +15,50 @@ module Legalese
 
     def run
       puts url
-
       report_privacy_policy
       report_tos
     end
 
-    def report_privacy_policy
-      msg = if srvice.has_privacy_policy?
+    private
+
+    def privacy_policy_message
+      if srvice.has_privacy_policy?
         srvice.privacy_policy_pages.first.url
       else
         "not found"
       end
-      puts "  privacy policy: #{msg}"
     end
 
-    def report_tos
-      msg = if srvice.has_tos?
+    def report_privacy_policy
+      puts "  privacy policy: #{privacy_policy_message}"
+    end
+
+    def tos_message
+      if srvice.has_tos?
         srvice.tos_pages.first.url
       else
         "not found"
       end
-      puts "  ToS: #{msg}"
+    end
 
+    def report_tos
+      puts "  ToS: #{tos_message}"
       if srvice.has_tos?
         report_tos_clauses(srvice)
       end
     end
 
+    def tos_clause_message(clause)
+      if srvice.contains_tos_clause?(clause)
+        "found"
+      else
+        "not found"
+      end
+    end
+
     def report_tos_clauses(srvice)
       TosPage.clauses.each do |clause|
-        msg = if srvice.contains_tos_clause?(clause)
-          "found"
-        else
-          "not found"
-        end
+        msg = tos_clause_message(clause)
         puts "    #{clause} clause: #{msg}"
       end
     end
