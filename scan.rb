90e1c3ec205c0844cc3f54bed808ff0af094fe41
Chris Praktish <chris.praktish@gmail.com>
2016-07-23 15:50:46 -0400

    Add sample file

--- scan.rb
+++ scan.rb
@@ -3,7 +3,9 @@ require_relative 'lib/legalese/reporter'
 
 urls = File.readlines('urls.txt')
 urls.map!(&:strip!)
+puts urls.count
 urls.reject!(&:empty?)
+puts urls.count
 urls.uniq!
 
 Legalese::Reporter.print_key
